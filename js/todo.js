05fd9da84c91a33ee8e0ff09fafd7d065a22df95
Hyeon Kim <stuttgart.kim@gmail.com>
2022-12-29 15:19:49 +0100

    HTML, JS

---
+++ js/todo.js
@@ -0,0 +1,66 @@
+const toDoForm = document.querySelector('#todo-form');
+const toDoInput = document.querySelector('#todo-form input');
+const toDoList = document.querySelector('#todo-list');
+
+const TODOS_KEY = 'todos';
+
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentNode;
+  li.remove();
+  console.log(typeof li.id);
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
+}
+
+function paintToDo(newTodo) {
+  const li = document.createElement('li');
+  li.id = newTodo.id;
+  const span = document.createElement('span');
+  span.innerText = newTodo.text;
+
+  const button = document.createElement('button');
+  button.innerText = '❌';
+  button.addEventListener('click', deleteToDo);
+
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = '';
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
+}
+
+toDoForm.addEventListener('submit', handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
+
+/*
+localStorage에는 array을 저장하지 못함.
+오직 텍스트만 저장 가능
+
+JSON.stringify()
+JavaScript Object나 array나 어떤 JavaScript 코드이건 간에
+그걸 string으로 바꿔주는 기능
+*/
